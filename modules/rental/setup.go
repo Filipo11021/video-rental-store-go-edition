d7f44bacca6dd43c38a217afffff3cae2347e6b5
Filip <>
2025-04-03 23:51:24 +0200

    fix(rental): add database auto-migration for Rental entity

--- modules/rental/setup.go
+++ modules/rental/setup.go
@@ -1,11 +1,22 @@
 package rental
 
-import "go.uber.org/fx"
+import (
+	"fmt"
+
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+)
 
 func Module() fx.Option {
 	return fx.Module("rental",
 		fx.Provide(newGormRentalRepo),
 		fx.Provide(newPriceCalculator),
 		fx.Provide(newApi),
+		fx.Invoke(func(db *gorm.DB) {
+			err := db.AutoMigrate(&Rental{})
+			if err != nil {
+				panic(fmt.Sprintf("failed to auto-migrate database: %v", err))
+			}
+		}),
 	)
 }
